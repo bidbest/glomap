8c4a80d79d3ac2234ee5c93389355ae8c22debbe
francesco bidoia <francesco.bidoia@gmail.com>
2024-08-11 19:18:49 +0900

    fixed git commit and lib path

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,11 @@
 # docker pull colmap/colmap:latest
 ./build.sh
-docker run --rm --gpus all -w /working -v $1:/working -it colmap:latest
+docker run --rm \
+	-e QT_XCB_GL_INTEGRATION=xcb_egl \
+    -e DISPLAY=:0 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -w /working \
+    -v $1:/working \
+    --gpus all \
+    --privileged \
+    -it colmap:latest
